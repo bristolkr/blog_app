1b44bbf84d4ce6334d67efc2cac9b59e67f28281
Kelly Bristol <bristolkr@gmail.com>
2015-06-09 15:28:01 -0400

    kermit flail

--- db/schema.rb
+++ db/schema.rb
@@ -17,24 +17,18 @@ ActiveRecord::Schema.define(version: 20150609180155) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "post_id"
-    t.string   "user"
-    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
-
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "image"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts"
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
-
